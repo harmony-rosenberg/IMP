c7a82252e2b27a9222cc07c4785309a1ad016550
harmony <harmony.rosenberg94@gmail.com>
2024-08-21 22:07:23 -0500

    added a get all albums route to retrieve every album in db

--- app/api/album_routes.py
+++ app/api/album_routes.py
@@ -4,9 +4,15 @@ from flask_login import login_required, current_user
 
 album_routes = Blueprint('albums', __name__, url_prefix='/albums')
 
+#get all albums
+@album_routes.route('')
+def get_all_albums():
+	albums = Album.query.all()
+	return jsonify([album.to_dict() for album in albums]), 200
+
 #get all albums for a specific user
-@album_routes.route('', methods=['GET'])
+@album_routes.route('/current')
 @login_required
-def get_albums():
+def get_user_albums():
 	albums = Album.query.filter_by(user_id=current_user.id).all()
 	return jsonify([album.to_dict() for album in albums]), 200
