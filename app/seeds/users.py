7b4a09734318b8a83583570d00d7515453297ade
harmony <harmony.rosenberg94@gmail.com>
2024-08-21 19:58:50 -0500

    added bio column to users table

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,9 +5,9 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+        username='Demo', email='demo@aa.io', password='password', bio='hey this is a test bio')
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        username='marnie', email='marnie@aa.io', password='password', bio='this is also a test bio')
     bobbie = User(
         username='bobbie', email='bobbie@aa.io', password='password')
 
@@ -28,5 +28,5 @@ def undo_users():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM users"))
-        
+
     db.session.commit()
