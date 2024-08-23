20421aed626f1b5f96169e2dbcc47a8f604fd16c
harmony <harmony.rosenberg94@gmail.com>
2024-08-22 19:24:13 -0500

    added create album api route

--- app/api/album_routes.py
+++ app/api/album_routes.py
@@ -16,3 +16,21 @@ def get_all_albums():
 def get_user_albums():
 	albums = Album.query.filter_by(user_id=current_user.id).all()
 	return jsonify([album.to_dict() for album in albums]), 200
+
+@album_routes.route('', methods=['POST'])
+@login_required
+def create_album():
+	album_title = request.json.get('albumTitle')
+	credits = request.json.get('credits')
+	artwork = request.json.get('artwork')
+
+	if not album_title:
+		return jsonify({'error': 'Title is required'})
+	if not artwork:
+		return jsonify({'error': 'Artwork is required'})
+
+	new_album = Album(album_title=album_title, credits=credits, artwork=artwork, user_id=current_user.id)
+	db.session.add(new_album)
+	db.session.commit()
+
+	return jsonify(new_album.to_dict()), 201
