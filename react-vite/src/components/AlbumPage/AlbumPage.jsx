a0bb77d465713471f871f978cd5c5ab165bdaaeb
harmony <harmony.rosenberg94@gmail.com>
2024-09-13 16:28:49 -0500

    fixed app crashing on album page as logged out user

--- react-vite/src/components/AlbumPage/AlbumPage.jsx
+++ react-vite/src/components/AlbumPage/AlbumPage.jsx
@@ -46,65 +46,86 @@ const AlbumPage = () => {
 	}
 
 	return (
-		isLoaded && user.id == selectedAlbum.user_id ?
-		(
-		<div className="album-details-container">
+		isLoaded && user ? (
+			isLoaded && user.id == selectedAlbum.user_id ?
+			(
+			<div className="album-details-container">
+				<h1>{selectedAlbum.album_title}</h1>
+				<img src={selectedAlbum.artwork} />
+				<div className="manage-btns">
+				<button className="delete-btn btn" onClick={openDeleteAlbumModal}>DELETE</button>
+				<button className="update-btn btn" onClick={openUpdateAlbumForm}>UPDATE</button>
+				<button className="upload-btn btn" onClick={openTrackUpload}>TRACKS</button>
+				</div>
+				<div className="track-list"> {Object.values(selectedAlbum.tracks).map((track) => (
+				<div key={track.id}>{track.track_title}</div>
+			))}
+			</div>
+			<div className="comments-container">
+					{Object.values(selectedAlbum.comments).map((comment) => (
+						<div
+						className="comment"
+						key={comment.id}>
+							<div className="comment-user">{comment.user.artistName} :</div>
+							{comment.body}
+							</div>
+					))}
+				</div>
+			</div>
+			) : isLoaded && selectedAlbum.user_id !== user.id ? (
+			<div className="album-details-container">
 			<h1>{selectedAlbum.album_title}</h1>
 			<img src={selectedAlbum.artwork} />
-			<div className="manage-btns">
-			<button className="delete-btn btn" onClick={openDeleteAlbumModal}>DELETE</button>
-			<button className="update-btn btn" onClick={openUpdateAlbumForm}>UPDATE</button>
-			<button className="upload-btn btn" onClick={openTrackUpload}>TRACKS</button>
-			</div>
 			<div className="track-list"> {Object.values(selectedAlbum.tracks).map((track) => (
-			<div key={track.id}>{track.track_title}</div>
-		))}
-		</div>
-		<div className="comments-container">
-				{Object.values(selectedAlbum.comments).map((comment) => (
-					<div
-					className="comment"
-					key={comment.id}>
-						<div className="comment-user">{comment.user.artistName} :</div>
-						{comment.body}
-						</div>
-				))}
+				<div key={track.id}>{track.track_title}</div>
+			))}
 			</div>
-		</div>
-		) : isLoaded && selectedAlbum.user_id !== user.id ? (
-		<div className="album-details-container">
-		<h1>{selectedAlbum.album_title}</h1>
-		<img src={selectedAlbum.artwork} />
-		<div className="track-list"> {Object.values(selectedAlbum.tracks).map((track) => (
-			<div key={track.id}>{track.track_title}</div>
-		))}
-		</div>
-		<form onSubmit={handleSubmit}>
-			<h2>say somethin</h2>
-		<div>
-			<textarea
-			rows={8}
-			cols={80}
-			value={body}
-			onChange={(e) => setBody(e.target.value)}
-			required
-			/>
-		</div>
-		<button type='submit'>post!</button>
-			</form>
+			<form onSubmit={handleSubmit}>
+				<h2>say somethin</h2>
+			<div>
+				<textarea
+				rows={8}
+				cols={80}
+				value={body}
+				onChange={(e) => setBody(e.target.value)}
+				required
+				/>
+			</div>
+			<button type='submit'>post!</button>
+				</form>
+				<div className="comments-container">
+					{Object.values(selectedAlbum.comments).map((comment) => (
+						<div
+						className="comment"
+						key={comment.id}>
+							<div className="comment-user">{comment.user.artistName} :</div>
+							{comment.body}
+							</div>
+					))}
+				</div>
+			</div>
+			) :
+				<h1>loading....</h1>
+		) : (
+			<div className="album-details-container">
+				<h1>{selectedAlbum.album_title}</h1>
+				<img src={selectedAlbum.artwork} />
+				<div className="track-list"> {Object.values(selectedAlbum.tracks).map((track) => (
+				<div key={track.id}>{track.track_title}</div>
+			))}
+			</div>
 			<div className="comments-container">
-				{Object.values(selectedAlbum.comments).map((comment) => (
-					<div
-					className="comment"
-					key={comment.id}>
-						<div className="comment-user">{comment.user.artistName} :</div>
-						{comment.body}
-						</div>
-				))}
+					{Object.values(selectedAlbum.comments).map((comment) => (
+						<div
+						className="comment"
+						key={comment.id}>
+							<div className="comment-user">{comment.user.artistName} :</div>
+							{comment.body}
+							</div>
+					))}
+				</div>
 			</div>
-		</div>
-		) :
-			<h1>loading....fart</h1>
+		)
 	)
 }
 
