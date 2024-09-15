e2299e7404a211d369671e7298d3a816bb4bc9f7
harmony <harmony.rosenberg94@gmail.com>
2024-09-15 15:25:45 -0500

    added alert for users to log in to view albums

--- react-vite/src/components/AlbumCard/AlbumCard.jsx
+++ react-vite/src/components/AlbumCard/AlbumCard.jsx
@@ -1,12 +1,19 @@
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from "react-redux";
 import './AlbumCard.css';
 
 const AlbumCard = ({album}) => {
 	const navigate = useNavigate()
+  const user = useSelector(state => state.session.user)
 
 	return (
 		<div className='card-container'>
-			<img onClick={() => navigate(`/albums/${album.id}`)} src={album.artwork} />
+			<img onClick={() => user ? (
+				navigate(`/albums/${album.id}`)
+			) : (
+				alert('log in to view albums!')
+			)}
+			src={album.artwork} />
 			<div>{album.album_title}</div>
 			<div>By {album.user.artistName}</div>
 			{/* <div>{album.genre} /  /<span className='date'>{date}</span></div> */}
