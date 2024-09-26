2b0ab8b31fa2e224eec870fb5f1f552a43a0e8e8
harmony <harmony.rosenberg94@gmail.com>
2024-09-26 15:02:31 -0500

    added album details page to house most of the content previously on album page

---
+++ react-vite/src/components/AlbumDetails/AlbumDetails.jsx
@@ -0,0 +1,59 @@
+import './AlbumDetails.css';
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, useNavigate } from 'react-router-dom';
+import { useModal } from "../../context/Modal";
+import CreateComment from "../CreateComment/CreateComment";
+import CommentCard from "../CommentCard/CommentCard";
+import DeleteAlbum from "../DeleteAlbum/DeleteAlbum";
+
+
+const AlbumDetails = ({album}) => {
+  const user = useSelector(state => state.session.user)
+	const navigate = useNavigate();
+  const { setModalContent } = useModal();
+	const albumId = album.id
+
+	const openDeleteAlbumModal = () => {
+		setModalContent(<DeleteAlbum album={album}/>)
+	}
+
+	const openUpdateAlbumForm = () => {
+		navigate(`/albums/${albumId}/edit`)
+	}
+
+	const openTrackUpload = () => {
+		navigate(`/albums/${albumId}/tracks`)
+	}
+
+	return (
+		user && user.id == album.user_id ? (
+			<div className="album-details-container">
+				<h1>{album.album_title}</h1>
+				<img src={album.artwork} />
+				<div className="manage-btns">
+				<button className="delete-btn btn" onClick={openDeleteAlbumModal}>DELETE</button>
+				<button className="update-btn btn" onClick={openUpdateAlbumForm}>UPDATE</button>
+				<button className="upload-btn btn" onClick={openTrackUpload}>TRACKS</button>
+				</div>
+				<div className="track-list"> {Object.values(album.tracks).map((track) => (
+				<div key={track.id}>{track.track_title}</div>
+			))}
+			</div>
+				<CommentCard album={album}/>
+			</div>
+			) :
+			<div className="album-details-container">
+			<h1>{album.album_title}</h1>
+			<img src={album.artwork} />
+			<div className="track-list"> {Object.values(album.tracks).map((track) => (
+				<div key={track.id}>{track.track_title}</div>
+			))}
+			</div>
+				<CreateComment album={album} />
+				<CommentCard album={album}/>
+			</div>
+
+	)
+}
+
+export default AlbumDetails;
--- react-vite/src/components/AlbumPage/AlbumPage.jsx
+++ react-vite/src/components/AlbumPage/AlbumPage.jsx
@@ -1,24 +1,13 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams, useNavigate } from 'react-router-dom';
-import { useModal } from "../../context/Modal";
+import { useParams } from 'react-router-dom';
 import { thunkGetAlbumDetails } from "../../redux/albums";
-import { thunkCreateComment } from '../../redux/comments';
-import DeleteAlbum from "../DeleteAlbum/DeleteAlbum";
 import './AlbumPage.css';
-import UpdateComment from "../UpdateComment";
-import OpenModalMenuItem from '../Navigation/OpenModalMenuItem';
-import DeleteComment from "../DeleteComment/DeleteComment";
-import CreateComment from "../CreateComment/CreateComment";
-import CommentCard from "../CommentCard/CommentCard";
-
+import AlbumDetails from "../AlbumDetails";
 
 const AlbumPage = () => {
 	const dispatch = useDispatch();
-	const navigate = useNavigate();
-  const { setModalContent } = useModal();
 	const {albumId} = useParams();
-	const [body, setBody] = useState('');
 	const albums = useSelector(state => state.albums)
 	const comments = useSelector(state => state.comments)
 	const [isLoaded, setIsLoaded] = useState(false);
@@ -29,68 +18,10 @@ const AlbumPage = () => {
 		dispatch(thunkGetAlbumDetails(albumId)).then(() => setIsLoaded(true))
 	}, [albumId, comments, dispatch, user])
 
-	const openDeleteAlbumModal = () => {
-		setModalContent(<DeleteAlbum album={selectedAlbum}/>)
-	}
-
-	const openUpdateAlbumForm = () => {
-		navigate(`/albums/${albumId}/edit`)
-	}
-
-	const openTrackUpload = () => {
-		navigate(`/albums/${albumId}/tracks`)
-	}
-
-	const handleSubmit = (e) => {
-		e.preventDefault()
-
-		const newComment = {
-			body,
-			album_id: albumId
-		}
-		dispatch(thunkCreateComment(newComment))
-		setBody("")
-		navigate(`/albums/${albumId}`)
-	}
-
 	return (
 		isLoaded ? (
-			user && user.id == selectedAlbum.user_id ? (
-			<div className="album-details-container">
-				<h1>{selectedAlbum.album_title}</h1>
-				<img src={selectedAlbum.artwork} />
-				<div className="manage-btns">
-				<button className="delete-btn btn" onClick={openDeleteAlbumModal}>DELETE</button>
-				<button className="update-btn btn" onClick={openUpdateAlbumForm}>UPDATE</button>
-				<button className="upload-btn btn" onClick={openTrackUpload}>TRACKS</button>
-				</div>
-				<div className="track-list"> {Object.values(selectedAlbum.tracks).map((track) => (
-				<div key={track.id}>{track.track_title}</div>
-			))}
-			</div>
-				<CommentCard album={selectedAlbum}/>
-			</div>
-			) : user && selectedAlbum.user_id !== user.id ? (
-			<div className="album-details-container">
-			<h1>{selectedAlbum.album_title}</h1>
-			<img src={selectedAlbum.artwork} />
-			<div className="track-list"> {Object.values(selectedAlbum.tracks).map((track) => (
-				<div key={track.id}>{track.track_title}</div>
-			))}
-			</div>
-				<CreateComment album={selectedAlbum} />
-				<CommentCard album={selectedAlbum}/>
-			</div>
+			<AlbumDetails album={selectedAlbum}/>
 			) :
-			<div className="album-details-container">
-				<h1>{selectedAlbum.album_title}</h1>
-				<img src={selectedAlbum.artwork} />
-				<div className="track-list"> {Object.values(selectedAlbum.tracks).map((track) => (
-				<div key={track.id}>{track.track_title}</div>
-			))}
-			</div>
-			</div>
-		) :
 			<h1>loading....</h1>
 	)
 }
