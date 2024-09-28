94a36f0a56d2af98872cd90958b15833d1a30e71
harmony <harmony.rosenberg94@gmail.com>
2024-09-27 21:05:15 -0500

    added users releases to profile

--- react-vite/src/components/ProfilePage/ProfilePage.jsx
+++ react-vite/src/components/ProfilePage/ProfilePage.jsx
@@ -1,17 +1,25 @@
 import './ProfilePage.css';
 import { useSelector } from "react-redux";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import AlbumCard from '../AlbumCard/AlbumCard';
 
 
 const ProfilePage = () => {
   const user = useSelector(state => state.session.user)
-	console.log(user)
+	const albums = useSelector(state => state.albums);
+
+	const userAlbums = Object.values(albums).filter(album => {
+		return album.user_id === user.id
+	})
 
 	return (
 		<>
-		<h1>{user.artistName}</h1>
+		<h1>{user.id}</h1>
 		<img className='pfp' src={user.profilePicture} />
 		<div>{user.bio}</div>
+		<div className='album-list'>Releases: {userAlbums.map((album) => (
+				<AlbumCard key={album.id} album={album}/>
+			))}</div>
 		</>
 	)
 }
